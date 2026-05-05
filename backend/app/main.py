8ed86cc3690fc63c1f56899eb4e18d68f89ba231
edgallagheris-lucyworksos <edgallagheris@googlemail.com>
2026-05-05 08:09:56 +0100

    Register Mail Ops routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from app.clinical_director_routes import router as clinical_director_router
 from app.dashboard_routes import router as dashboard_router
 from app.domain_routes import router as domain_router
 from app.episode_state_routes import router as episode_state_router
+from app.mail_ops_routes import router as mail_ops_router
 from app.operating_routes import router as operating_router
 from app.safety_routes import router as safety_router
 
@@ -11,4 +12,5 @@ app.include_router(operating_router)
 app.include_router(dashboard_router)
 app.include_router(clinical_director_router)
 app.include_router(episode_state_router)
+app.include_router(mail_ops_router)
 app.include_router(safety_router)
