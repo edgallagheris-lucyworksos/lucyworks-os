f9da673d840f7abe617694d6b4cccd2e0fba8c80
edgallagheris-lucyworksos <edgallagheris@googlemail.com>
2026-06-19 17:04:48 +0100

    Rewrite resource directory to resources

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: { externalDir: true },
+  async rewrites() {
+    return [
+      {
+        source: "/resource-directory",
+        destination: "/resources",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
