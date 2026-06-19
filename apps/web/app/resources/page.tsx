3935c306419528ccee58e943c48a837a2321258a
edgallagheris-lucyworksos <edgallagheris@googlemail.com>
2026-06-19 17:02:29 +0100

    Show assignment directory on resources page

--- apps/web/app/resources/page.tsx
+++ apps/web/app/resources/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AssignmentDirectoryManager } from "@/components/assignment-directory-manager";
 import { AuthGuard } from "@/components/auth-guard";
 import { HospitalShell } from "@/components/hospital-shell";
 import { LucyWorksCommandSurface } from "@/components/lucyworks-command-surface";
@@ -9,6 +10,7 @@ export default function ResourcesPage() {
     <AuthGuard allowedRoles={["ops_manager", "clinical_director", "clinician", "nurse", "admin"]}>
       {(user) => (
         <HospitalShell title="RESOURCES" subtitle="rooms, staff, theatre, imaging, pharmacy">
+          <AssignmentDirectoryManager />
           <LucyWorksCommandSurface mode="resources" user={user} />
         </HospitalShell>
       )}
