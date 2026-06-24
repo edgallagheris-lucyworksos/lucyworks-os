6605788edf8b17c1d5673595a9efbfc8526985fb
edgallagheris-lucyworksos <edgallagheris@googlemail.com>
2026-06-24 09:47:14 +0100

    Run staff location grid validation

--- scripts/validate_lucyworks_architecture.py
+++ scripts/validate_lucyworks_architecture.py
@@ -4,6 +4,7 @@ import sys
 checks = [
     "scripts/validate_current_architecture.py",
     "scripts/validate_work_area_boards.py",
+    "scripts/validate_staff_location_grid.py",
 ]
 
 for check in checks:
